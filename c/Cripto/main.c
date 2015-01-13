d425609f2c98be95c63147ca82ed7bfbeeb07881
Victor <victorestevetavares@hotmail.com>
2015-01-13 18:13:53 +0100

    Modificacción para metrizar el funcionamiento

--- c/Cripto/main.c
+++ c/Cripto/main.c
@@ -11,15 +11,34 @@
 /*
  * 
  */
-int main(int argc, char** argv) {
-    char caracter;
-    char num = ('k'-'a');
+int main( int argc, char** argv ) {
+    int caracter;
+    int num = ('k'-'a');
+    int i;
+    
+    if (argc >= 2) {
+        if(argv[1][0] >= 'a' && argv[1][0] <= 'z' )
+            num = ( argv[1][0] - 'a' );
+        if(argv[1][0] >= 'A' && argv[1][0] <= 'Z' )
+            num = ( argv[1][0] - 'A' );
+    }
+    
+    for(i = 0; i < argc; i++) {
+        printf("%s\r\n", argv[i]);
+    }
+    printf("\r\n");
     
     while( (caracter = getchar()) != EOF ) {
         if( caracter >= 'a' && caracter <= 'z') {
             caracter = caracter + num;
             if(caracter > 'z'){
-                caracter = (caracter - 'z' + 'a' - (char)1) ;
+                caracter = (caracter - 'z' + 'a' - 1) ;
+            }
+        }
+        else if( caracter >= 'A' && caracter <= 'Z') {
+            caracter = caracter + num;
+            if(caracter > 'Z'){
+                caracter = (caracter - 'Z' + 'A' - 1) ;
             }
         }
         putchar(caracter);
