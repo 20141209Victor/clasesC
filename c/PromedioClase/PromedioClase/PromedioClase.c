15788e5c4947a222854750015e79226e52e49f7b
Victor <victorestevetavares@hotmail.com>
2015-02-04 17:00:36 +0100

    primera version

--- c/PromedioClase/PromedioClase/PromedioClase.c
+++ c/PromedioClase/PromedioClase/PromedioClase.c
@@ -12,8 +12,9 @@
  * 
  */
 int main(int argc, char** argv) {
-    float nota, notaMedia, notaMaxima = 0;
-    int noPromediables = 0, suspensos = 0, dudosos = 0, aprobados = 0, alumnoNotaMaxima = 0;
+    float nota, notaMedia;
+    float notaMaxima[2] = {0.0,0.0};
+    int noPromediables = 0, suspensos = 0, dudosos = 0, aprobados = 0;
     int numero;
     
     printf("cuantos alumnos hay en clase? " );    
@@ -24,29 +25,31 @@ int main(int argc, char** argv) {
         float clase[numero][3];
         int i = 0;
         
-        //este bucle for sirve para pedir al usuario todas las notas y rellenar con estos valores la tabla clase
+        //este bucle for sirve para pedir al usuario todas las notas y rellenar con estos valores la tabla "clase"
         for(i = 0; i < numero; i++) {
             clase[i][0] = i+1;
 
-            printf("introducir la 1ª nota del alumno %d", i+1);
-            scanf("%d", &clase[i][1]);
+            printf("introducir la 1ª nota del alumno %d -> ", i+1);
+            scanf("%f", &clase[i][1]);
             // introducimos un contror de errores de insercion
             while( clase[i][1] < 0 || clase[i][1] > 10) {
                 printf("      valor incorrecto \r\n");
-                printf("introducir la 1ª nota del alumno %d", i+1);
-                scanf("%d", &clase[i][1]);
+                printf("introducir la 1ª nota del alumno %d -> ", i+1);
+                scanf("%f", &clase[i][1]);
             }
             
-            printf("introducir la 2ª nota del alumno %d", i+1);
-            scanf("%d", &clase[i][2]);
+            printf("introducir la 2ª nota del alumno %d -> ", i+1);
+            scanf("%f", &clase[i][2]);
             // introducimos un contror de errores de insercion
             while( clase[i][2] < 0 || clase[i][2] > 10) {
                 printf("      valor incorrecto \r\n");
-                printf("introducir la 2ª nota del alumno %d", i+1);
-                scanf("%d", &clase[i][2]);
+                printf("introducir la 2ª nota del alumno %d -> ", i+1);
+                scanf("%f", &clase[i][2]);
             }
         }
         
+        //este bucle for sirve para leer la tabala "clase" y por cada estudiante 
+        // calcular la media y poner a cada estudiante en una clasificacción
         for(i = 0; i < numero; i++){
             printf("Alumno numero %d: \t nota 1: %f \t nota 2: %f \t", i+1, clase[i][1], clase[i][2]);
             notaMedia = (clase[i][1] + clase[i][2]) / 2;
@@ -69,14 +72,20 @@ int main(int argc, char** argv) {
                 aprobados++;
             }
             
-            
-            ..............
-            
+            if( notaMaxima[1] < notaMedia ) {
+                notaMaxima[1] = notaMedia;
+                notaMaxima[0] = clase[i][0];
+            }           
         }
-        
     }
     
+    printf("La nota maxima es: %f del alumno %f \r\n", notaMaxima[1], notaMaxima[0]);
+    printf("Alumnos no promediables: %d que es el %f%% \r\n", noPromediables, (float)noPromediables/numero*100 );
+    printf("Alumnos suspendidos: %d que es el %f%% \r\n", suspensos, (float)suspensos/numero*100 );
+    printf("Alumnos dudosos: %d que es el %f%% \r\n", dudosos, (float)dudosos/numero*100 );
+    printf("Alumnos aprobados: %d que es el %f%% \r\n", aprobados, (float)aprobados/numero*100 );
     
+
     return (EXIT_SUCCESS);
 }
 
